3b1e875679674242058030bc93b7cba1b5923f54
SAL-BEI <salaashkaelo@gmail.com>
2025-01-22 14:11:41 +0300

    created NewsAPI instance

---
+++ app/src/main/java/com/example/mynewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.mynewsapp.util
+
+class Constants {
+    companion object{
+
+        const val API_KEY = "19876197aa244393b3dd6136a2fdfcc5"
+        const val BASE_URL="https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY="500L"
+        const val  QUERY_PAGE_SIZE="20"
+    }
+}
