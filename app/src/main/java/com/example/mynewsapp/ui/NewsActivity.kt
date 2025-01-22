1a502a646d68797e9246fae8a9e8be26c4f4b713
SAL-BEI <salaashkaelo@gmail.com>
2025-01-22 12:25:03 +0300

    renamed main activity

--- app/src/main/java/com/example/mynewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/mynewsapp/ui/NewsActivity.kt
@@ -1,17 +1,20 @@
-package com.example.mynewsapp
+package com.example.mynewsapp.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.newsprojectpractice.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_news)
+        //changed findViewById(R.id.main) to findViewById(R.id.flFragment)
+        //to remove errors
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.flFragment)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
