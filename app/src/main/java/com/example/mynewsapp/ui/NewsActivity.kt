6e841d4458966894033bbfc3974adfcf8c8c9147
SAL-BEI <salaashkaelo@gmail.com>
2025-01-27 10:39:39 +0300

    `feat: Implement NewsActivity with ViewModel and navigation setup

--- app/src/main/java/com/example/mynewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/mynewsapp/ui/NewsActivity.kt
@@ -3,21 +3,39 @@ package com.example.mynewsapp.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.mynewsapp.db.ArticleDatabase
+import com.example.mynewsapp.repository.NewsRepository
 import com.example.newsprojectpractice.R
+import com.example.newsprojectpractice.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var newsViewModel: NewsViewModel
+    private lateinit var binding: ActivityNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize the binding
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Enable edge-to-edge display
         enableEdgeToEdge()
-        setContentView(R.layout.activity_news)
-        //changed findViewById(R.id.main) to findViewById(R.id.flFragment)
-        //to remove errors
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.flFragment)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Initialize the ViewModel
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        newsViewModel =
+            ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+        // Set up navigation
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
