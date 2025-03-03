2437418b82bb2f6a7a4eabbd46bf063d0cfc2591
SAL-BEI <salaashkaelo@gmail.com>
2025-03-03 12:54:40 +0300

    implemented the progress bar and pushed the snack bar to the top of the page

--- app/src/main/java/com/example/mynewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/mynewsapp/ui/fragments/ArticleFragment.kt
@@ -2,6 +2,8 @@ package com.example.mynewsapp.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -13,9 +15,9 @@ import com.google.android.material.snackbar.Snackbar
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
-    lateinit var newsViewModel: NewsViewModel
-    val args: ArticleFragmentArgs by navArgs()
-    lateinit var binding: FragmentArticleBinding
+    private lateinit var newsViewModel: NewsViewModel
+    private val args: ArticleFragmentArgs by navArgs()
+    private lateinit var binding: FragmentArticleBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,22 +26,38 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         newsViewModel = (activity as NewsActivity).newsViewModel
         val article = args.article
 
+        // Show ProgressBar, Hide WebView Initially
+        binding.progressBar.visibility = View.VISIBLE
+        binding.webView.visibility = View.GONE
+
         binding.webView.apply {
             webViewClient = WebViewClient()
-            article.url.let {
-                loadUrl(it)
-
+            webChromeClient = object : WebChromeClient() {
+                override fun onProgressChanged(view: android.webkit.WebView?, newProgress: Int) {
+                    if (newProgress == 100) {
+                        // Hide ProgressBar and Show WebView when fully loaded
+                        binding.progressBar.visibility = View.GONE
+                        binding.webView.visibility = View.VISIBLE
+                    }
+                }
             }
-
+            article.url?.let {
+                loadUrl(it)
+            }
         }
 
         binding.fab.setOnClickListener {
             newsViewModel.addToFavorites(article)
-            Snackbar.make(view, "Added to favorites", Snackbar.LENGTH_SHORT).show()
 
+            // Create Snackbar and Move it to the Top
+            val snackbar = Snackbar.make(view, "Added to favorites", Snackbar.LENGTH_SHORT)
+            val snackbarView = snackbar.view
+            val params = snackbarView.layoutParams as ViewGroup.MarginLayoutParams
 
+            params.setMargins(0, 0, 0, 1450) // Move Snackbar 100dp from the top
+            snackbarView.layoutParams = params
+
+            snackbar.show()
         }
     }
-
 }
-
